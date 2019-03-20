a00a010ffb1660835eac04dc51ab44b3c92d8f11
Gamze ŞAHİN <gamzeshn04@hotmail.com>
2019-03-20 18:11:33 +0300

    o

---
+++ izibiz.Application/izibiz.MODEL/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using izibiz.MODEL.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace izibiz.MODEL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Invoice> sentInvoices { get; set; }
+
+        public DbSet<Invoice> incomingInvoices { get; set; }
+
+        public DbSet<Invoice> draftInvoices { get; set; }
+
+
+       
+    }
+}
